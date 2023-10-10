39c5d67495f2b392845dc249b790f3b262d69594
Gowri442005 <144794114+Gowri442005@users.noreply.github.com>
2023-10-10 20:21:05 +0530

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,45 @@
+class Batsman:
+    def _init_(self):
+        self.strike_rate = 0.0
+        self.total_runs = 0
+        self.highest_score = 0
+        self.batting_rank = 0
+    def get_bat(self,sr,tr,hs,br):
+        self.strike_rate = sr
+        self.total_runs = tr
+        self.highest_score = hs
+        self.batting_rank = br
+    def disp_bat(self):
+        print "\nBATTING DATA\n"
+        print "Strike Rate:",self.strike_rate
+        print "Total Runs:",self.total_runs
+        print "Highest Score:",self.highest_score
+        print "Batting Rank:",self.batting_rank      
+class Bowler:
+    def _init_(self):
+        self.wickets_taken = 0
+        self.economy = 0.0
+        self.hattricks = 0
+        self.bowling_rank = 0
+    def get_bowl(self,wt,ec,ht,bor):
+        self.wickets_taken = wt
+        self.economy = ec
+        self.hattricks = ht
+        self.bowling_rank = bor
+    def disp_bowl(self):
+        print "\nBOWLING DATA\n"
+        print "Wickets Taken:",self.wickets_taken
+        print "Economy:",self.economy
+        print "Hattricks:",self.hattricks
+        print "Bowling Rank:",self.bowling_rank
+class AllRounder(Batsman,Bowler):
+    def _init_(self):
+        Batsman._init_(self)
+        Bowler._init_(self)
+        self.allrounder_rank = 0
+    def get_all(self,sr,tr,hs,br,wt,ec,ht,bor,ar):
+        Batsman.get_bat(self,sr,tr,hs,br)
+        Bowler.get_bowl(self,wt,ec,ht,bor)
+        self.allrounder_rank = ar
+    def disp_all(self):
+        print "\nALL-ROUNDER DATA"
